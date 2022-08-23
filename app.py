9d92e81ad886e38b9da9014a465308a2c2693472
alanrom <romeroa@MacBook-Air-de-Jesus.local>
2022-08-23 01:36:23 -0500

    adding htpps

--- app.py
+++ app.py
@@ -3,9 +3,6 @@ from flask import Flask, jsonify, render_template
 from flask_talisman import Talisman
 
 app = Flask(__name__)
-csp = {
-    'default-src': '\'self\''
-}
 Talisman(app)
 
 @app.route('/')
